d64d4582f5e219fbde6c42fd1fd9c9f5d26aa8df
pgq18 <12110122@mail.sustech.edu.cn>
2023-07-27 21:37:30 +0800

    cnt

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 import os
-
 from flask import Flask, request, jsonify
+
+cnt = 0
 
 app = Flask(__name__)
 
 @app.route('/set', methods = ['GET','POST'])
 def set():
-    return jsonify({'text' : 'done'}) , 200
+    cnt = cnt + 1
+    return jsonify({'text' : str(cnt)}) , 200
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
