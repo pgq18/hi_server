e1b8be5771c3c512f0dfa01dd7e977b90a0c5f16
pgq18 <12110122@mail.sustech.edu.cn>
2023-07-27 22:23:56 +0800

    to http

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, jsonify
+import requests
 
 cnt = 0
 
@@ -9,7 +10,8 @@ app = Flask(__name__)
 def set():
     global cnt
     cnt = cnt + 1
-    return jsonify({'text' : str(cnt)}) , 200
+    respons = requests.post("tcp://1.tcp.cpolar.cn:23058/set", data={"text" : "hello!"}).json()["text"]
+    return jsonify({'text' : respons}) , 200
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
