1c1c490614265467c65bd2c830968eb6170bbd89
pgq18 <12110122@mail.sustech.edu.cn>
2023-07-28 04:50:21 +0800

    add get

--- app/main.py
+++ app/main.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 
 @app.route('/set', methods = ['GET','POST'])
 def set():
-    data_in = request.json
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post("https://4172e3b2.r3.cpolar.cn/set", headers=headers, data = json.dumps(data_in)).json()["text"]
-    return jsonify({'text' : response}) , 200
+    if request.method == "POST":
+        data_in = request.json
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post("https://4172e3b2.r3.cpolar.cn/set", headers=headers, data = json.dumps(data_in)).json()["text"]
+        return jsonify({'text' : response}) , 200
+    if request.method == "Get":
+        response = requests.get("https://4172e3b2.r3.cpolar.cn/set").json()
+        return jsonify({'text' : response}) , 200
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
