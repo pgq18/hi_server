ea3c10b3b342834bc45b084231036d9d02aa14e9
pgq18 <12110122@mail.sustech.edu.cn>
2023-07-27 21:46:09 +0800

    re

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/set', methods = ['GET','POST'])
 def set():
     cnt = cnt + 1
-    return jsonify({'text' : str(cnt)}) , 200
+    return jsonify({'text' : 'done'}) , 200
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
