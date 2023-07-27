db9a9367227d165f4a9f4bfc07692e0f9ebed529
pgq18 <12110122@mail.sustech.edu.cn>
2023-07-27 21:19:29 +0800

    set

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return '欢迎使用微信云托管！'
+@app.route('/set', methods = ['GET','POST'])
+def set():
+    return jsonify({'text' : 'done'}) , 200
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
