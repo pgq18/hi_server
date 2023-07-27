d872899665927083bdd90cb084eceeda5341632a
pgq18 <12110122@mail.sustech.edu.cn>
2023-07-28 02:35:58 +0800

    data

--- app/main.py
+++ app/main.py
@@ -2,16 +2,13 @@ import os
 from flask import Flask, request, jsonify
 import requests
 
-cnt = 0
-
 app = Flask(__name__)
 
 @app.route('/set', methods = ['GET','POST'])
 def set():
-    global cnt
-    cnt = cnt + 1
-    respons = requests.post("https://4172e3b2.r3.cpolar.cn/set", data={"text" : "hello!"}).json()["text"]
-    return jsonify({'text' : respons}) , 200
+    data_in = request.json()
+    response = requests.post("https://4172e3b2.r3.cpolar.cn/set", data = data_in).json()["text"]
+    return jsonify({'text' : response}) , 200
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
