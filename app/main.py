a7f1e0464cf4633a9801bb6858c9872375c6a08f
pgq18 <12110122@mail.sustech.edu.cn>
2023-07-28 16:33:10 +0800

    ppp

--- app/main.py
+++ app/main.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 @app.route('/set', methods = ['GET','POST'])
 def set():
     data_in = request.json
+    headers = {'Content-Type': 'application/json'}
     if request.method == "POST":
         print("post")
-        headers = {'Content-Type': 'application/json'}
         response = requests.post("https://16006eb3.r3.cpolar.cn/set", headers=headers, data=json.dumps(data_in)).json()["text"]
         return jsonify({'text' : response}) , 200
     if request.method == "GET":
         print("get")
-        response = requests.get("https://16006eb3.r3.cpolar.cn/set", data=json.dumps(data_in)).json()
+        response = requests.get("https://16006eb3.r3.cpolar.cn/set", headers=headers, data=json.dumps(data_in)).json()
         return jsonify(response) , 200
 
 if __name__ == "__main__":
