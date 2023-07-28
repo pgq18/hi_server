3de9b0d6f1ed007a5a9eb1b8cf5cc475c28c1612
pgq18 <12110122@mail.sustech.edu.cn>
2023-07-28 17:03:42 +0800

    test

--- app/main.py
+++ app/main.py
@@ -8,12 +8,14 @@ app = Flask(__name__)
 @app.route('/set', methods = ['GET','POST'])
 def set():
     data_in = request.json
+    print("json")
     headers = {'Content-Type': 'application/json'}
     if request.method == "POST":
         print("post")
         response = requests.post("https://16006eb3.r3.cpolar.cn/set", headers=headers, data=json.dumps(data_in)).json()["text"]
         return jsonify({'text' : response}) , 200
     if request.method == "GET":
+        return jsonify({'text' : "get_done"}) , 200
         print("get")
         response = requests.get("https://16006eb3.r3.cpolar.cn/set", headers=headers, data=json.dumps(data_in)).json()
         return jsonify(response) , 200
