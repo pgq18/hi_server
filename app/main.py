6a708e264f86626207dd574b65d0cb245567a91b
pgq18 <12110122@mail.sustech.edu.cn>
2023-07-29 11:04:44 +0800

    get

--- app/main.py
+++ app/main.py
@@ -5,19 +5,23 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/set', methods = ['GET','POST'])
+@app.route('/set', methods = ['POST'])
 def set():
     data_in = request.json
     print("json")
     headers = {'Content-Type': 'application/json'}
-    if request.method == "POST":
-        print("post")
-        response = requests.post("https://16006eb3.r3.cpolar.cn/set", headers=headers, data=json.dumps(data_in)).json()["text"]
-        return jsonify({'text' : response}) , 200
-    if request.method == "GET":
-        print("get")
-        response = requests.get("https://16006eb3.r3.cpolar.cn/set", headers=headers, data=json.dumps(data_in)).json()
-        return jsonify(response) , 200
+    print("set")
+    response = requests.post("https://primate-major-gecko.ngrok-free.app/set", headers=headers, data=json.dumps(data_in)).json()["text"]
+    return jsonify({'text' : response}) , 200
+
+@app.route('/get', methods = ['POST'])
+def get():
+    data_in = request.json
+    print("json")
+    headers = {'Content-Type': 'application/json'}
+    print("get")
+    response = requests.post("https://primate-major-gecko.ngrok-free.app/get", headers=headers, data=json.dumps(data_in)).json()
+    return jsonify(response) , 200
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
